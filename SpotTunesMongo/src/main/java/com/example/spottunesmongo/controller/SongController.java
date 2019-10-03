a95f71cfe1f69f45c9d24546932a7c698d6dc5f8
Christine Le <christine.le244@gmail.com>
2019-10-03 17:19:43 -0400

    can perform CRUD on songs table

--- SpotTunesMongo/src/main/java/com/example/spottunesmongo/controller/SongController.java
+++ SpotTunesMongo/src/main/java/com/example/spottunesmongo/controller/SongController.java
@@ -1,4 +1,31 @@
 package com.example.spottunesmongo.controller;
 
+import com.example.spottunesmongo.model.Song;
+import com.example.spottunesmongo.services.SongServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/songs")
 public class SongController {
+
+    @Autowired
+    SongServices songServices;
+
+    @GetMapping("/list")
+    public List<Song> listAllSongs() {
+        return songServices.listSongs();
+    }
+
+    @PostMapping("/add")
+    public Song addSong(@RequestBody Song song) {
+        return songServices.addSong(song);
+    }
+
+    @DeleteMapping("/delete/id")
+    public String deleteSong(@PathVariable String id) {
+        return songServices.deleteSong(id);
+    }
 }
