29308c7f2d88e587db6c5fe7e19e32b7070b6c57
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-10-03 16:35:03 -0400

    Added spottunes mongodb project

---
+++ SpotTunesMongo/src/main/java/com/example/spottunesmongo/SpottunesmongoApplication.java
@@ -0,0 +1,13 @@
+package com.example.spottunesmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpottunesmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpottunesmongoApplication.class, args);
+	}
+
+}
