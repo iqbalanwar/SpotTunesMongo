ee119165e34a1dc32d9876a57f0dfc570ddc9475
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-10-03 20:47:35 -0400

    Comment

--- SpotTunesMongo/src/main/java/com/example/spottunesmongo/services/SongServices.java
+++ SpotTunesMongo/src/main/java/com/example/spottunesmongo/services/SongServices.java
@@ -17,8 +17,10 @@ public interface SongServices {
     public Song addSong(Song song);
 
     // delete song by title
+    // I get this is dumb, but this is just to see
+    // Ofc it's better by id. It's w/e
     public String deleteSong(String title);
 
-    // Just some CRUD operation
+    // Just some CRUD operation on the title too
     public Song updateTitle(String title, String newTitle);
 }
